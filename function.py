5758107c8b48bc267bb40e1b4808a31796b62431
Zeus <72276901+ZeusDevv@users.noreply.github.com>
2023-02-18 16:15:47 +0100

    Create function.py

---
+++ function.py
@@ -0,0 +1,23 @@
+import asyncio
+import discord
+
+async def create_logs(ctx):
+  await ctx.message.delete()
+  print("Attempting To Create All Channels Now")
+  category_id = category_id # Change this to your category ID
+  guild_id = guild_id # Change this to your guild (Discord Server) ID
+  guild = discord.utils.get(self.client.guilds, id=guild_id)
+  category = discord.utils.get(guild.categories, id=category_id)
+  channel_names = [    'default',    'testwebhook',    'playermoney',    'playerinventory',    'robbing',    'cuffing',    'drop',    'trunk',    'stash',    'glovebox',    'banking',    'vehicleshop',    'vehicleupgrades',    'shops',    'dealers',    'storerobbery',    'bankrobbery',    'powerplants',    'death',    'joinleave',    'ooc',    'report',    'me',    'pmelding',    '112',    'bans',    'anticheat',    'weather',    'moneysafes',    'bennys',    'bossmenu',    'robbery',    'casino',    'traphouse',    '911',    'palert',    'house',]
+  webhooks = {}
+  for name in channel_names:
+    channel = await guild.create_text_channel(name, category=category)
+    webhook = await channel.create_webhook(name=name, reason="Create webhook for channel")
+    webhooks[name] = webhook.url
+    with open("webhook_links.txt", "w") as f:
+      for name, url in webhooks.items():
+        print(f"New Entry | ['{name}'] = '{url}',\n")
+        f.write(f"['{name}'] = '{url}',\n")
+      print("Finished")
+
+      await asyncio.sleep(1)  # Sleep for 1 second to avoid rate limiting
